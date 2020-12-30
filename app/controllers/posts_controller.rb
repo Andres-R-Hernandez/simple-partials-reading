a5f0c735167654fb902e750ffb66d138643aad70
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2020-12-30 11:57:31 -0600

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,9 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @author = @post.author
   end
+
 
   def new
     @post = Post.new
@@ -30,5 +32,5 @@ class PostsController < ApplicationController
     @post.update(title: params[:title], description: params[:description])
     redirect_to post_path(@post)
   end
-  
+
 end
